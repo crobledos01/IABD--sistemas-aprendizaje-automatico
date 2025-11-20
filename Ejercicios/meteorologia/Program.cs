d4af367cf264b435da68a30d00ac186ee33e8b84
Carlos <crobledos01@educarex.es>
2025-11-20 20:26:03 +0100

    creado el programa de meteorologia

---
+++ Ejercicios/meteorologia/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace meteorologia
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
