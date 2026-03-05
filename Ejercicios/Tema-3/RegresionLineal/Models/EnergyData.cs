b26dc19ad4df09648c78d3ef86b4d5d856b7356d
Carlos <crobledos01@educarex.es>
2026-03-05 20:31:00 +0100

    varias

---
+++ Ejercicios/Tema-3/RegresionLineal/Models/EnergyData.cs
@@ -0,0 +1,23 @@
+namespace RegresionLineal.Models;
+
+public class EnergyData
+{
+    //[LoadColumn(0)]
+    public float TempC { get; set; }
+
+    //[LoadColumn(1)]
+    public float HumidityPct { get; set; }
+
+    //[LoadColumn(2)]
+    public float PressureBar { get; set; }
+
+    //[LoadColumn(3)]
+    public float LoadPct { get; set; }
+
+    //[LoadColumn(4)]
+    public float Vibration { get; set; }
+
+    //[LoadColumn(5)]
+    public float EnergyKWh { get; set; }
+
+}
---
+++ Ejercicios/Tema-3/RegresionLineal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RegresionLineal
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
