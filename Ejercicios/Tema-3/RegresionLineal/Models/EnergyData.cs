40705ea0b5eb8bd919301fa2d38856f042902a7b
Carlos <crobledos01@educarex.es>
2026-03-12 20:25:39 +0100

    regresión lineal

--- Ejercicios/Tema-3/RegresionLineal/Models/EnergyData.cs
+++ Ejercicios/Tema-3/RegresionLineal/Models/EnergyData.cs
@@ -1,23 +1,24 @@
+using Microsoft.ML.Data;
 namespace RegresionLineal.Models;
 
 public class EnergyData
 {
-    //[LoadColumn(0)]
+    [LoadColumn(0)]
     public float TempC { get; set; }
 
-    //[LoadColumn(1)]
+    [LoadColumn(1)]
     public float HumidityPct { get; set; }
 
-    //[LoadColumn(2)]
+    [LoadColumn(2)]
     public float PressureBar { get; set; }
 
-    //[LoadColumn(3)]
+    [LoadColumn(3)]
     public float LoadPct { get; set; }
 
-    //[LoadColumn(4)]
+    [LoadColumn(4)]
     public float Vibration { get; set; }
 
-    //[LoadColumn(5)]
+    [LoadColumn(5)]
     public float EnergyKWh { get; set; }
 
 }
